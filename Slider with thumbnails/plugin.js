de29c16d29c5594f013d50ebb0ae15b959f31b9c
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2022-01-09 02:54:58 +0200

    mm-exp2 last changes

--- Slider with thumbnails/plugin.js
+++ Slider with thumbnails/plugin.js
@@ -1,4 +1,159 @@
 $(document).ready(function () {
+  $("body").append(`<script
+    type="text/javascript"
+    src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script
+    type="text/javascript"
+    src="//code.jquery.com/jquery-migrate-1.2.1.min.js"
+    ></script>
+    <script
+    type="text/javascript"
+    src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"
+    ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>`);
+  $(".product_section .product__images .product-gallery").hide();
+  $(".product_section .product__images").append(`
+        <div class="carousel_kh carousel-main">
+        <div class="optimize_carousel_cell">
+            <img
+            src="//cdn.shopify.com/s/files/1/1762/4231/products/Beef-Jerky-Flower-Bouquet_2048_compressed_1200x.jpg?v=1627803667"
+            alt="Beef Jerky Flower Bouquet"
+            />
+        </div>
+        <div class="optimize_carousel_cell">
+            <img
+            src="//cdn.shopify.com/s/files/1/1762/4231/products/beef-jerky-flower-bouquet-unboxing-2048-optimized_1200x.jpg?v=1627803958"
+            alt="Beef Jerky Flower Bouquet"
+            />
+        </div>
+        <div class="optimize_carousel_cell">
+            <img
+            src="//cdn.shopify.com/s/files/1/1762/4231/products/beef-jerky-flower-bouquet-unboxing-2-2048px-optimized_1200x.jpg?v=1627803777"
+            alt="Beef Jerky Flower Bouquet"
+            />
+        </div>
+        <div class="optimize_carousel_cell">
+            <img
+            src="//cdn.shopify.com/s/files/1/1762/4231/products/beef-jerky-flower-bouquet-14435002318931_1200x.png?v=1627803667"
+            alt="Beef Jerky Flower Bouquet"
+            />
+        </div>
+        <div class="optimize_carousel_cell">
+            <img
+            src="https://i.ibb.co/mTVLpS1/Group-29.png"
+            alt="Beef Jerky Flower Bouquet"
+            />
+        </div>
+        <div class="optimize_carousel_cell">
+            <img
+            src="https://i.ibb.co/M27hJKm/Group-35.png"
+            alt="Beef Jerky Flower Bouquet"
+            />
+        </div>
+        </div>
+        <div class="carousel_thumbnails">
+        <div class="carousel-thumb active">
+            <img
+            data-index="0"
+            src="//cdn.shopify.com/s/files/1/1762/4231/products/Beef-Jerky-Flower-Bouquet_2048_compressed_1200x.jpg?v=1627803667"
+            alt="Beef Jerky Flower Bouquet"
+            />
+        </div>
+        <div class="carousel-thumb">
+            <img
+            data-index="1"
+            src="//cdn.shopify.com/s/files/1/1762/4231/products/beef-jerky-flower-bouquet-unboxing-2048-optimized_1200x.jpg?v=1627803958"
+            alt="Beef Jerky Flower Bouquet"
+            />
+        </div>
+        <div class="carousel-thumb">
+            <img
+            data-index="2"
+            src="//cdn.shopify.com/s/files/1/1762/4231/products/beef-jerky-flower-bouquet-unboxing-2-2048px-optimized_1200x.jpg?v=1627803777"
+            alt="Beef Jerky Flower Bouquet"
+            />
+        </div>
+        <div class="carousel-thumb">
+            <img
+            data-index="3"
+            src="//cdn.shopify.com/s/files/1/1762/4231/products/beef-jerky-flower-bouquet-14435002318931_1200x.png?v=1627803667"
+            alt="Beef Jerky Flower Bouquet"
+            />
+        </div>
+        <div class="carousel-thumb">
+            <img
+            data-index="4"
+            src="https://i.ibb.co/mTVLpS1/Group-29.png"
+            alt="Beef Jerky Flower Bouquet"
+            />
+        </div>
+        <div class="carousel-thumb">
+            <img
+            data-index="5"
+            src="https://i.ibb.co/M27hJKm/Group-35.png"
+            alt="Beef Jerky Flower Bouquet"
+            />
+        </div>
+        </div>
+    </div>
+    <div class="optimize_carousel_popup">
+      <a
+        href="//cdn.shopify.com/s/files/1/1762/4231/products/Beef-Jerky-Flower-Bouquet_2048_compressed_1200x.jpg?v=1627803667"
+        data-fancybox="gallery"
+      >
+        <img
+          class="fancybox-image"
+          src="//cdn.shopify.com/s/files/1/1762/4231/products/Beef-Jerky-Flower-Bouquet_2048_compressed_1200x.jpg?v=1627803667"
+          alt="Beef Jerky Flower Bouquet"
+        />
+      </a>
+      <a
+        href="//cdn.shopify.com/s/files/1/1762/4231/products/beef-jerky-flower-bouquet-unboxing-2048-optimized_1200x.jpg?v=1627803958"
+        data-fancybox="gallery"
+      >
+        <img
+          class="fancybox-image"
+          src="//cdn.shopify.com/s/files/1/1762/4231/products/beef-jerky-flower-bouquet-unboxing-2048-optimized_1200x.jpg?v=1627803958"
+          alt="Beef Jerky Flower Bouquet"
+        />
+      </a>
+      <a
+        href="//cdn.shopify.com/s/files/1/1762/4231/products/beef-jerky-flower-bouquet-unboxing-2-2048px-optimized_1200x.jpg?v=1627803777"
+        data-fancybox="gallery"
+      >
+        <img
+          class="fancybox-image"
+          src="//cdn.shopify.com/s/files/1/1762/4231/products/beef-jerky-flower-bouquet-unboxing-2-2048px-optimized_1200x.jpg?v=1627803777"
+          alt="Beef Jerky Flower Bouquet"
+        />
+      </a>
+      <a
+        href="//cdn.shopify.com/s/files/1/1762/4231/products/beef-jerky-flower-bouquet-14435002318931_1200x.png?v=1627803667"
+        data-fancybox="gallery"
+      >
+        <img
+          class="fancybox-image"
+          src="//cdn.shopify.com/s/files/1/1762/4231/products/beef-jerky-flower-bouquet-14435002318931_1200x.png?v=1627803667"
+          alt="Beef Jerky Flower Bouquet"
+        />
+      </a>
+      <a href="https://i.ibb.co/mTVLpS1/Group-29.png" data-fancybox="gallery">
+        <img
+          class="fancybox-image"
+          src="https://i.ibb.co/mTVLpS1/Group-29.png"
+          alt="Beef Jerky Flower Bouquet"
+        />
+      </a>
+      <a href="https://i.ibb.co/M27hJKm/Group-35.png" data-fancybox="gallery">
+        <img
+          class="fancybox-image"
+          src="https://i.ibb.co/M27hJKm/Group-35.png"
+          alt="Beef Jerky Flower Bouquet"
+        />
+      </a>
+    </div>
+  `);
+
   var $carousel = $(".carousel_kh");
   $carousel.slick({
     lazyLoad: "ondemand",
