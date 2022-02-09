0e121cacab8bc3d84d347f899bdc6771535d0521
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2022-02-10 01:11:05 +0200

    RTA-EXP-02.js

---
+++ RTA/RTA-EXP-02/RTA-EXP-02.js
@@ -0,0 +1,85 @@
+
+
+setTimeout(() => {
+  convert.$('#content > div > div > section.elementor-section.elementor-top-section.elementor-element.elementor-element-64a13486.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default > div > div.elementor-column.elementor-col-50.elementor-top-column.elementor-element.elementor-element-2078da73 > div > section.elementor-section.elementor-inner-section.elementor-element.elementor-element-15a646f3.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default > div > div > div > div > div').append(`<ul class="elementor-icon-list-items convert_custom_design_ul">
+  <li class="elementor-icon-list-item">
+    <span class="elementor-icon-list-icon">
+      <i aria-hidden="true" class="fas fa-check"></i
+    ></span>
+    <span class="elementor-icon-list-text"
+      >Customize <span class="bold">your own design</span></span
+    >
+  </li>
+  <li class="elementor-icon-list-item">
+    <span class="elementor-icon-list-icon">
+      <i aria-hidden="true" class="fas fa-check"></i
+    ></span>
+    <span class="elementor-icon-list-text"
+      ><span class="bold">Endless ammenities</span>to choose from</span
+    >
+  </li>
+  <li class="elementor-icon-list-item">
+    <span class="elementor-icon-list-icon">
+      <i aria-hidden="true" class="fas fa-check"></i>
+    </span>
+    <span class="elementor-icon-list-text"
+      >Create<span class="bold">Create a special layout</span> to call your
+      own</span
+    >
+  </li>
+  <li class="elementor-icon-list-item">
+    <span class="elementor-icon-list-icon">
+      <i aria-hidden="true" class="fas fa-check"></i
+    ></span>
+    <span class="elementor-icon-list-text"
+      >Build a design fit to<span class="bold">your unique needs</span></span
+    >
+  </li>
+</ul>`)
+  convert._$('DIV.elementor-element.elementor-element-675af20a.elementor-align-left.elementor-icon-list--layout-traditional.elementor-list-item-link-full_width.elementor-widget.elementor-widget-icon-list > DIV.elementor-widget-container').append(`<ul class="elementor-icon-list-items convert_best_selling_ul">
+  <li class="elementor-icon-list-item">
+    <span class="elementor-icon-list-icon">
+      <i aria-hidden="true" class="fas fa-check"></i
+    ></span>
+    <span class="elementor-icon-list-text"
+      >Select from <span class="bold">tried and true designs</span></span
+    >
+  </li>
+  <li class="elementor-icon-list-item">
+    <span class="elementor-icon-list-icon">
+      <i aria-hidden="true" class="fas fa-check"></i
+    ></span>
+    <span class="elementor-icon-list-text"
+      >Pre-designed <span class="bold">by professionals</span></span
+    >
+  </li>
+  <li class="elementor-icon-list-item">
+    <span class="elementor-icon-list-icon">
+      <i aria-hidden="true" class="fas fa-check"></i>
+    </span>
+    <span class="elementor-icon-list-text"
+      >A simple and <span class="bold">stress-free process</span>stress-free process</span
+    >
+  </li>
+  <li class="elementor-icon-list-item">
+    <span class="elementor-icon-list-icon">
+      <i aria-hidden="true" class="fas fa-check"></i
+    ></span>
+    <span class="elementor-icon-list-text"
+      >Easy to assemble <span class="bold">kitchen layouts</span></span
+    >
+  </li>
+</ul>`)
+
+
+
+
+  convert._$('DIV.elementor-widget-wrap.elementor-element-populated > DIV.elementor-element.elementor-element-178fc1b9.elementor-widget.elementor-widget-html').children().hide();
+  convert._$('DIV.elementor-widget-wrap.elementor-element-populated > DIV.elementor-element.elementor-element-178fc1b9.elementor-widget.elementor-widget-html').append(`
+  <div class="convert_img">
+     <img src="https://i.ibb.co/KGZNJnF/image-3.png" alt="custom design">
+  </div>
+`);
+
+
+}, 1500);
