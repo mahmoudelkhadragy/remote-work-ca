ab511e8b981c06de41dc6cc9ca3b80fb44cabd21
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2022-02-10 21:54:15 +0200

    lf-exp1 changes

--- living-fit/LF-EXP1/lf-exp1.js
+++ living-fit/LF-EXP1/lf-exp1.js
@@ -34,3 +34,31 @@ document.querySelector('.opt_header').insertAdjacentHTML("beforebegin", styles);
 document.querySelectorAll('._submit').forEach(el => el.textContent = 'Get Free Workouts');
 document.querySelectorAll('form._form').forEach(el => el.style.background = 'transparent !important');
 document.querySelector('body').insertAdjacentHTML("beforeend", styles);
+
+
+$('.opt_custom_submit').on('click', function () {
+  let opt_name = $('.opt_input_name').val();
+  let opt_email = $('.opt_input_email').val();
+
+  if (validateEmail(opt_email)) {
+    $('#fullname').val(opt_name);
+    $('#email').val(opt_email);
+    $('#_form_5_submit').click();
+    setTimeout(() => {
+      let thank_you_message = $('._form-thank-you').clone();
+      $('.opt_form_cutom_here').append(thank_you_message);
+      $('.opt_cutom_form').hide();
+    }, 1300);
+  } else {
+    $('.opt_cutom_form').append('<p class="opt_message mt-2 text-center">required valid email</p>');
+  }
+
+});
+
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
