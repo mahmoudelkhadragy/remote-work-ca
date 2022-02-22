f1ddc76c9e8cb99aa6f1f694db522b0d7c676005
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2022-02-22 02:42:35 +0200

    ascacs

--- Astrology/HS-AP-EXP9/HS-AP-EXP9-variant1.js
+++ Astrology/HS-AP-EXP9/HS-AP-EXP9-variant1.js
@@ -319,6 +319,22 @@ $(document).ready(function () {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css">
   `);
 
+  $('.woocommerce-product-gallery').before(`<div class="opt_main_mobile"></div>`);
+
+  $('.opt_main_mobile').append($('.product_title.entry-title').clone());
+  $('.opt_main_mobile').append($('.woocommerce-product-details__short-description').clone());
+
+
+  $(".optimize_join").on("click", function () {
+    var scrollTo = $("form.variations_form.cart").offset().top - 20;
+    $("html, body").animate(
+      {
+        scrollTop: scrollTo,
+      },
+      700
+    );
+  });
+
 
 
   // form slider
@@ -586,4 +602,42 @@ $(document).ready(function () {
 
 
 
+});
+
+
+
+
+var $carousel = $(".carousel_kh");
+$carousel.slick({
+  lazyLoad: "ondemand",
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: `<span class="prev_arrow slider_arrows"
+    ><svg class="flickity-button-icon" viewBox="0 0 100 100">
+      <path
+        d="M95.04 46 21.68 46 48.18 22.8 42.91 16.78 4.96 50 42.91 83.22 48.18 77.2 21.68 54 95.04 54 95.04 46z"
+        class="arrow"
+      ></path></svg>
+  </span>`,
+  nextArrow: `<span class="next_arrow slider_arrows"><svg class="flickity-button-icon" viewBox="0 0 100 100"><path d="M95.04 46 21.68 46 48.18 22.8 42.91 16.78 4.96 50 42.91 83.22 48.18 77.2 21.68 54 95.04 54 95.04 46z" class="arrow" transform="translate(100, 100) rotate(180) "></path></svg></span>`,
+});
+
+// carousel thumbnail click
+$(".carousel-thumb img").on("click", function () {
+  var index = +$(this).data("index");
+  $carousel.slick("slickGoTo", index, false);
+
+  $(this).parent().siblings().removeClass("active");
+  $(this).parent().addClass("active");
+});
+
+$("[data-fancybox]").fancybox({
+  animationEffect: "tube",
+});
+
+$(".optimize_carousel_cell img").on("click", function () {
+  var imgSrc = $(this).attr("src");
+  $(`.optimize_carousel_popup a[href="${imgSrc}"]`).click();
 })
+
+
--- Astrology/HS-AP-EXP9/fancy.js
+++ Astrology/HS-AP-EXP9/fancy.js
@@ -1,4 +1,42 @@
-<section class="optimize_what">
+setTimeout(() => {
+  $("body").append(`
+  <script
+  type="text/javascript"
+  src="//code.jquery.com/jquery-1.11.0.min.js"
+  ></script>
+  <script
+    type="text/javascript"
+    src="//code.jquery.com/jquery-migrate-1.2.1.min.js"
+  ></script>
+  <script
+    type="text/javascript"
+    src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"
+  ></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>`
+  );
+
+  // appned styles to the head
+  $('head').append(`
+  <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css">
+  `);
+
+  $('head').append(`
+  <style>
+  .optimize_carousel_popup {
+    display: none;
+  }
+  .optimize_carousel_popup a {
+    width: 40%;
+  }
+  .optimize_carousel_popup a img {
+    width: 100%;
+  } 
+  </style>
+  `);
+
+  $('#main').after(`
+  <section class="optimize_what">
   <h2 class="h2_title">What Does Astrology+ Have to Offer?</h2>
   <p class="description">
     Discover more about your past, present, and future with premium readings and
@@ -8,7 +46,7 @@
     <div class="opt_product">
       <div class="opt_img_product carousel_kh">
         <img
-          src="https://shop.astrology.com/wp-content/uploads/sites/5/2020/03/Natal-Moon-Report-768x768.png"
+          src="https://shop.astrology.com/wp-content/uploads/sites/5/2020/03/Natal-Moon-Report-300x300.png"
           alt="natal moon report"
         />
         <img
@@ -85,7 +123,7 @@
 
 <div class="optimize_carousel_popup">
   <a
-    href="https://shop.astrology.com/wp-content/uploads/sites/5/2020/03/Natal-Moon-Report-768x768.png"
+    href="https://shop.astrology.com/wp-content/uploads/sites/5/2020/03/Natal-Moon-Report-300x300.png"
     data-fancybox="gallery"
   >
     <img
@@ -150,11 +188,37 @@
     />
   </a>
 </div>
-<!-- 
-https://i.ibb.co/yPhrZFj/Natal-Moon-Report-min.png
-https://i.ibb.co/Q98s5kb/2022-Premium-Horoscope-min.png
-https://i.ibb.co/7ghZhd1/Birth-Horoscope-Natal-Chart-min.png
 
--->
 
-https://i.ibb.co/Db7LpcQ/outline-navigate-next-black-24dp.png
+  `);
+  setTimeout(() => {
+    var $carousel = $(".carousel_kh");
+    $carousel.slick({
+      lazyLoad: "ondemand",
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 580,
+          settings: {
+            arrows: true,
+            prevArrow: `<span class="slider_arrows"><img src="https://i.ibb.co/FWSQhVN/outline-navigate-before-black-24dp.png" alt="back"></span>`,
+            nextArrow: `<span class="slider_arrows"><img src="https://i.ibb.co/Db7LpcQ/outline-navigate-next-black-24dp.png" alt="next"></span>`
+          }
+        }
+      ]
+    });
+
+    $("[data-fancybox]").fancybox({
+      animationEffect: "tube",
+    });
+
+    $(".opt_img_product img").on("click", function () {
+      var imgSrc = $(this).attr("src");
+      console.log(imgSrc);
+      $(`.optimize_carousel_popup a[href="${imgSrc}"]`).click();
+    });
+  }, 2000);
+}, 4000)
