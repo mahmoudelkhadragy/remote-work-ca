59aece1acbcb98619a8b7c7b9ba125e69eae1293
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2022-03-06 07:14:07 +0200

    HS-Tarout

---
+++ Astrology/HS-Tarot-EXP10/index.js
@@ -0,0 +1,91 @@
+var opt_cards = [
+  {
+    id: 'javascript:TarotDeckSubmitTarotCard(1);',
+    cardSubTitle: 'The Magician',
+    cardImg: 'https://www.astrology.com/images-US/tarot/results/tarot-reading-result-1.svg',
+    no: `<b>NO</b><br>You have gone too far in believing in your own power. A fall, decline, or minor humiliation awaits along this path. You have lessons that you have not learned, and you have insulted or angered a higher being. You must restore equilibrium to the elements and acknowledge yourself as a beginner. Then you will be prepared for a new attempt.`,
+    yes: `<b>YES</b><br>Space and time will not prevent you from realizing your vision for the situation. Your inner power serves and protects the good in a touchy situation. Be open to what the light reveals to you, and you will emerge victorious. The Magician brings boldness and foresight, so seek within for the deeper meaning. The alchemical wedding commences when you open yourself to insight.`,
+  },
+  {
+    id: 'javascript:TarotDeckSubmitTarotCard(2);',
+    cardSubTitle: 'The High Priestess',
+    cardImg: 'https://www.astrology.com/images-US/tarot/results/tarot-reading-result-2.svg',
+    no: `<b>NO</b><br>You suffer from a lack of willpower or resolution and follow conflicting pieces of advice. You display childlike ignorance about the basic forces at work in the situation. You will not succeed quite yet, or at least not in the way that you want. You must take a step backward and re-evaluate the situation. In time, you will come to understand why things have not been going your way.`,
+    yes: `<b>YES</b><br>The High Priestess signifies a changing situation. Like the phases of the Moon, the Priestess undergoes continual change. The change in progress works in your favor. Your opportunity will arrive soon, so allow yourself to drift with the current and you will arrive at a favorable destination. Keep your goal in mind, but do not force it to come about. Simply allow the situation to work for your benefit.`,
+  },
+  {
+    id: 'javascript:TarotDeckSubmitTarotCard(3);',
+    cardSubTitle: 'The Empress',
+    cardImg: 'https://www.astrology.com/images-US/tarot/results/tarot-reading-result-3.svg',
+    no: `<b>NO</b><br>You have hesitated to make decisions and lost the opportune moment. You have wished for the best but have failed to act. The Empress shows you that you must seize the day if you want to succeed in life. She sits on the throne but can lose her place through inaction. Better to act than to lose the moment.`,
+    yes: `<b>YES</b><br>The Empress enthroned reveals that your good actions in the past are now bearing fruit. Remember your hard work and good deeds months ago? The situation bears the marks of just rewards. You can rest contented and smile, as you deserve everything that you have coming to you. Sit upon the throne and let your beauty, strength, and wisdom show.`,
+  },
+  {
+    id: 'javascript:TarotDeckSubmitTarotCard(4);',
+    cardSubTitle: 'The Emperor',
+    cardImg: 'https://www.astrology.com/images-US/tarot/results/tarot-reading-result-4.svg',
+    no: `<b>NO</b><br>The Emperor right now shows that you have allowed your emotions to get out of control. You have criticized someone close to you, someone who is loyal and only wishes good things for you. You must step down from your throne and apologize in order to make things right. Only humility will make a difference as you calmly and carefully address the conflict.`,
+    yes: `<b>YES</b><br>The Emperor is a card of victory and conquest. You have asserted your will upon the world, and you will prevail. The inner, divine nature triumphs over superficial matters. Mars and the Sun defeat Venus and Saturn. Go forward with full confidence in your abilities and do not hold anything back. Ignore any doubt that might arise and move forward assertively.`,
+  },
+  {
+    id: 'javascript:TarotDeckSubmitTarotCard(5);',
+    cardSubTitle: 'The Pope',
+    cardImg: 'https://www.astrology.com/images-US/tarot/results/tarot-reading-result-5.svg',
+    no: `<b>NO</b><br>The negative side of the Pope card shows weakness of will and an excess of kindness. You have allowed others to get the better of you through your naïve and trusting nature. You have neglected your own priorities in order to concentrate on what others want. In this particular situation, a little bit of selfishness will yield a better outcome. It is OK to take care of yourself.`,
+    yes: `<b>YES</b><br>The Pope card, sometimes called the Hierophant, signifies divine wisdom and manifestation of blessing. You look deep within to bring the heavenly light to shed clarity on the situation. Others will be inspired by your loving, merciful outlook on life. You have deep reserves of peace and tranquility that take you above this present moment. You bless yourself as you bless others.`,
+  },
+  {
+    id: 'javascript:TarotDeckSubmitTarotCard(6);',
+    cardSubTitle: 'The Lovers',
+    cardImg: 'https://www.astrology.com/images-US/tarot/results/tarot-reading-result-6.svg',
+    no: `<b>NO</b><br>The negative side of the Lovers shows a love that has been lost or a test that has been failed. Every good thing has its shadow side, and every gain has a hidden loss. This time around, things happen to not go the way that you thought or hoped. It is OK to be sad for a time, since loving and losing is part of life.`,
+    yes: `<b>YES</b><br>The Lovers signify inspiration and sometimes help from the spirit world. A test or trial comes your way that you will pass. Someone who is no longer in your life will help you psychically. Be open to receive the intuition, which seems impossible but is there anyway. Be thankful for the kindness of the guides who light your way, and remember to do the same for others when your time comes.`,
+  },
+  {
+    id: 'javascript:TarotDeckSubmitTarotCard(7);',
+    cardSubTitle: 'The Chariot',
+    cardImg: 'https://www.astrology.com/images-US/tarot/results/tarot-reading-result-7.svg',
+    no: `<b>NO</b><br>A change in the direction of fate means that your plans will not be fulfilled - for now at least. As swift as the wind, the Chariot that once took you to victory now brings you low. Do not be afraid, for sometimes what seems like a loss carries good things in its path. The negative trend will be short lived, and you will find yourself in a better position in a short while.`,
+    yes: `<b>YES</b><br>Good health and success come your way as the Chariot brings you victory! You have the ability to overcome obstacles that have stood in your way for years. Your friends come to your aid, and your enemies flee. The wheels of fate have rolled forward, and you find yourself on top of your world. Enjoy this favorable sign, and have confidence.`,
+  },
+  // You have misunderstood the situation and have employed bad strategy. The Chariot implies impulsiveness and lack of control. Your impatience has gotten the better of you, and the situation will not go your way. You must learn to think on your feet so that you do not make as many mistakes. Leave yourself a better future by acting deliberately and going with your intuition.
+  {
+    id: 'javascript:TarotDeckSubmitTarotCard(8);',
+    cardSubTitle: 'Justice',
+    cardImg: 'https://www.astrology.com/images-US/tarot/results/tarot-reading-result-8.svg',
+    no: `<b>NO</b><br>The Justice card means that your conscience is bothering you because of some wrong that you committed in the past. A person who seems like an enemy brings your actions into the light. If you can get over your ego nature, you will see the path clearly. You must freely admit what you did wrong, and only then will your plans proceed smoothly.`,
+    yes: `<b>YES</b><br>Someone who wronged you in the past will now repay you in a positive sense. The Justice card means that an imbalance will be righted, a bad situation made good, a crooked road made straight. You deserve to have this correction, since it results from your own good deeds in the past. Allow your mind to rest and breathe a deep sigh of relief.`,
+  },
+  // The Justice card in its negative sense means that an imbalance has come into your life. Someone close to you will lose trust in your words because of your own practice of being less than truthful. You have strayed from the path, and you must return to good ways in order to restore your relationship, which is very important for your future evolution.
+  {
+    id: 'javascript:TarotDeckSubmitTarotCard(9);',
+    cardSubTitle: 'The Hermit',
+    cardImg: 'https://www.astrology.com/images-US/tarot/results/tarot-reading-result-9.svg',
+    no: `<b>NO</b><br>The Hermit, in its negative sense, means an excess of caution and a harsh form of withdrawal from the world. You have blessings in your life that you are not seeing, and you have allowed this fact to turn you into a lonely soul. A truly wise person knows how to balance the worldly life with the life of the spirit.`,
+    yes: `<b>YES</b><br>The Hermit wanders alone, indicating inspired action and lack of concern for material things. Today, this card indicates great inner power and strength... the ability to see things through without regard for the circumstances. You will surely succeed in your endeavor, but sacrifice and commitment will be required. The staff of discipline will guide you.`,
+  },
+  //The negative side of the Hermit card indicates a failure to take action in the world. You have not been decisive because of an excess of concern for the spiritual world. You have wavered in your worldly commitments, which is not a good thing in this case. You must see your duty properly and carry it through, or your plans will be ruined. 
+  {
+    id: 'javascript:TarotDeckSubmitTarotCard(11);',
+    cardSubTitle: 'Strength',
+    cardImg: 'https://www.astrology.com/images-US/tarot/results/tarot-reading-result-11.svg',
+    no: `<b>NO</b><br>This card indicates Strength that is not under the guidance of good judgment. You have a tendency to force your way through situations that are not to your liking. This can result in severed ties with potential allies. Learn to be more adaptable and flexible. Seek out the wisdom of a feminine figure. Only then will you succeed.`,
+    yes: `<b>YES</b><br>The Strength card reveals that you face formidable obstacles, but you have the resources within you to overcome whatever comes your way. You have great courage radiating from your heart center, and you have many blessings in your life. As you take an inventory of the resources available to you, you will see that the situation is no cause for worry.`,
+  },
+
+
+
+
+]
+
+
+
+// var random_boolean = Math.random() < 0.5;
+
+// $(".deck a").each(function(){
+//   $(this).attr("id", ($(this).attr("onclick")).match(/[0-9]/));
+//   $(this).prop("onclick", null).off("click");
+// });
+// $(".deck a").on("click", function(){
+//   console.log($(this).attr("id"))
+// })
