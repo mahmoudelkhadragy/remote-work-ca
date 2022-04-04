e9ad3d64c01f8e3dd40beef17132387487c804e0
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2022-04-04 03:51:39 +0200

    wkt-001-init

---
+++ weekett/WKT-001/index.js
@@ -0,0 +1,37 @@
+$('head').append(`
+<style>
+#slickSlide10 > div.slideshow__text-wrap.slideshow__text-wrap--desktop > div > div > ul > li > h2{
+  font-family: 'SofiaProLight' !important;
+}
+.opt_wkt_wrraper {
+  width: 80%;
+}
+.opt_wkt_link{
+  text-align: center;
+  display: inline-block;
+  background-color: #FECF0A;
+  color: #003050;
+  padding: 12px 0;
+  border-radius: 20px;
+  transition: all .2s ease-in-out;
+}
+.opt_wkt_link:hover{
+  color: #fff;
+  background-color: #436076;
+}
+</style>
+`);
+
+$('#slickSlide10 > div.slideshow__text-wrap.slideshow__text-wrap--desktop > div > div > ul > li > h2').html(`
+Get More <b>“Me Time”</b>
+<br> 
+at Tea Time
+`);
+$('#slickSlide10 > div.slideshow__text-wrap.slideshow__text-wrap--desktop > div > div > ul > li > h2').after(`
+<div class="opt_wkt_wrraper">
+ <p class="opt_wkt_p">Pair with your phone or smart home device to have hot water ready for every afternoon tea break.</p>
+ <a class="opt_wkt_link" href="https://www.weekett.com/cart/add?id=17261508526150&quantity=1">Buy Now</a>
+ </div>
+`);
+
+
