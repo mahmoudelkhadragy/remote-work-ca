8e971267c496718573d0e6ffa0bf8ffef8f6ec37
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2022-06-16 19:31:55 +0200

    BWW-003

---
+++ BWW/BWW-003/index.js
@@ -0,0 +1,469 @@
+convert._$('DIV#find-exchange').css('background-color', '#004897');
+convert._$('DIV#find-exchange').css('background-image', 'url(\'https://i.ibb.co/gS6hd5R/bww-back.png\')');
+
+
+
+convert.$(document).ready(function () {
+  convert.$('#find-exchange').append(`<div class="con_header">
+  <h2>Get Started with Bitcoin the Smart & Secure Way</h2>
+  <p>
+    Learn more about investing in crypto, discover verified exchanges to trade
+    on, and find the safest ways to store your Bitcoin.
+  </p>
+  <div class="con_form_container"></div>
+</div>`);
+  convert.$('.con_form_container').append(convert.$('form.form-inline'));
+  convert.$('#find-exchange form.form-inline h2.map-title-2').text('with');
+  convert.$('#find-exchange form.form-inline').prepend(`<h2 class="con_subhead">Buy Bitcoin or Crypto in:</h2>`);
+});
+$('.post-content').before(`
+<section class="con_welcome">
+  <div class="con_container">
+    <div class="row">
+      <div class="col-sm-12 col-md-7">
+        <div class="con_welcome_info">
+          <p>
+            Welcome to Buy Bitcoin Worldwide! I’m Jordan Tuwiner, the founder of
+            this site.
+          </p>
+          <p>
+            We understand that buying bitcoins can be extremely confusing and
+            frustrating. Luckily for you, this site has ample information to
+            help make buying bitcoins easier for you.
+          </p>
+          <p>Keep reading!</p>
+          <div class="con_welcome_info_profile">
+            <div class="con_profile_img">
+              <img
+                src="https://i.ibb.co/7yRt3Zr/con-profile.png"
+                alt="profile photo"
+              />
+            </div>
+            <div class="con_welcome_profile">
+              <h2>Jordan Tuwiner Founder,</h2>
+              <h3>Buy Bitcoin Worldwide</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-12 col-md-5">
+        <div class="con_welcome_img">
+          <img
+            src="https://i.ibb.co/KF4t3PF/con-welcome.png"
+            alt="Bitcoin photo"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="con_popular">
+  <div class="con_container">
+    <h2>Most Popular Guides</h2>
+    <div class="row">
+      <div class="con_popular_item_wrapper">
+        <a
+          class="con_popular_item"
+          href="https://www.buybitcoinworldwide.com/#how-to-buy-bitcoins"
+        >
+          <img src="https://i.ibb.co/B3zk3Qy/con-guid1.png" alt="guid1" />
+          <p>Where and How to Buy Bitcoins</p>
+        </a>
+      </div>
+      <div class="con_popular_item_wrapper">
+        <a
+          class="con_popular_item"
+          href="https://www.buybitcoinworldwide.com/credit-debit-card/"
+        >
+          <img src="https://i.ibb.co/sR3r1r3/con-guid2.png" alt="guid2" />
+          <p>How to Buy with Credit / Debit Card</p>
+        </a>
+      </div>
+      <div class="con_popular_item_wrapper">
+        <a
+          class="con_popular_item"
+          href="https://www.buybitcoinworldwide.com/en/buy-bitcoin-paypal/"
+        >
+          <img src="https://i.ibb.co/6YsjNSC/con-guid3.png" alt="guid3" />
+          <p>How to Buy with Paypal</p>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="con_about">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 col-md-4">
+        <div class="con_about_start">
+          <h2>
+            New to Bitcoin? <br />
+            <strong>Get Started Here!</strong>
+          </h2>
+          <p>
+            Our goal is to help you get informed about Bitcoin so you can make
+            decisions confidently and navigate the complex world of
+            cryptocurrency. Take a look at our most popular sections here to get
+            started.
+          </p>
+        </div>
+      </div>
+      <div class="col-sm-12 col-md-8">
+        <div class="con_about_learn">
+          <div class="row">
+            <div class="col-sm-12 col-md-6">
+              <div class="con_learn_img">
+                <h3>Learn About Bitcoin</h3>
+                <img
+                  src=" https://i.ibb.co/bPvzLgp/con-about2.png"
+                  alt="about1"
+                />
+              </div>
+            </div>
+            <div class="col-sm-12 col-md-6">
+              <div class="con_learn_info">
+                <a
+                  href="https://www.buybitcoinworldwide.com/#how-to-buy-bitcoins"
+                  >How to Buy Bitcoins</a
+                >
+                <a href="https://www.buybitcoinworldwide.com/wallets/"
+                  >About Wallets</a
+                >
+                <a
+                  href="https://www.buybitcoinworldwide.com/who-accepts-bitcoin/"
+                  >Who Accepts Bitcoins?</a
+                >
+                <a
+                  href="https://www.buybitcoinworldwide.com/how-many-bitcoins-are-there/"
+                  >How Many Bitcoins Are There?</a
+                >
+                <a href="https://www.buybitcoinworldwide.com/bitcoin-clock/"
+                  >Bitcoin Clock</a
+                >
+                <a
+                  href="https://www.buybitcoinworldwide.com/how-many-bitcoin-users/"
+                  >How Many People Buy and Use Bitcoin?</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 col-md-8">
+        <div class="con_about_learn">
+          <div class="row">
+            <div class="col-sm-12 col-md-6">
+              <div class="con_learn_img">
+                <h3>Find An Exchange</h3>
+                <img
+                  src="https://i.ibb.co/vHxdyjz/con-about1.png"
+                  alt="about2"
+                />
+              </div>
+            </div>
+            <div class="col-sm-12 col-md-6">
+              <div class="con_learn_info">
+                <a href="https://www.buybitcoinworldwide.com/united-states/">United States</a>
+                <a href="https://www.buybitcoinworldwide.com/europe/">Europe</a>
+                <a href="https://www.buybitcoinworldwide.com/canada/">Canada</a>
+                <a href="https://www.buybitcoinworldwide.com/united-kingdom/">United Kingdom</a>
+                <a href="https://www.buybitcoinworldwide.com/australia/">Australia</a>
+                <a href="https://www.buybitcoinworldwide.com/new-zealand/">New Zealand</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-12 col-md-4">
+        <div class="con_about_start">
+          <div class="con_about_start_head">
+            <img src="https://i.ibb.co/X4mW2qR/con-about3.png" alt="about3" />
+            <h2>Buy Crypto</h2>
+          </div>
+          <div class="con_learn_info">
+            <a href="https://www.buybitcoinworldwide.com/credit-debit-card/">Using a Credit/Debit Card</a>
+            <a href="https://www.buybitcoinworldwide.com/buy-bitcoin-bank-transfer-account/">With a Bank Transfer</a>
+            <a href="https://www.buybitcoinworldwide.com/en/buy-bitcoin-paypal/">Using Paypal</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+`);
+$('head').append(`
+<style>
+@import url("http://fonts.cdnfonts.com/css/proxima-nova-2?styles=44818,44820,44812");
+/* general */
+.post-content {
+  display: none;
+}
+.con_container {
+  width: 100%;
+  max-width: 970px;
+  padding: 0 10px;
+  margin: auto;
+}
+/* general */
+
+.con_welcome {
+  padding-top: 50px;
+}
+.con_welcome .row {
+  display: flex;
+  align-items: stretch;
+}
+.con_welcome .row > .col-sm-7 {
+  display: flex;
+  align-items: center;
+}
+.con_welcome_info {
+  border-left: 6px solid #2479c6;
+  padding: 10px 10px 10px 30px;
+}
+.con_welcome_info p {
+  font-size: 20px;
+  font-family: "Proxima Nova", sans-serif;
+  font-weight: 400;
+  color: #1a1a1a;
+}
+.con_welcome_info p:nth-child(2) {
+  padding: 10px 0;
+}
+.con_welcome_profile {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+}
+.con_welcome_profile h2,
+.con_welcome_profile h3 {
+  font-family: "Proxima Nova", sans-serif;
+  font-size: 18px;
+  margin: 0;
+}
+.con_welcome_info_profile {
+  display: flex;
+  padding-top: 20px;
+}
+.con_welcome_info_profile .con_profile_img {
+  padding-right: 15px;
+}
+.con_welcome_profile h2 {
+  font-weight: 400;
+  color: #1a1a1a;
+  padding-bottom: 7px;
+}
+.con_welcome_profile h3 {
+  color: #1366c4;
+}
+@media (max-width: 767px) {
+  .con_welcome .row {
+    flex-direction: column;
+  }
+  .con_welcome_img img {
+    display: block;
+    margin: auto;
+  }
+  .con_welcome_img {
+    padding-top: 15px;
+  }
+}
+
+.con_popular {
+  background: rgb(51, 131, 222);
+  background: linear-gradient(
+    90deg,
+    rgba(51, 131, 222, 1) 0%,
+    rgba(19, 102, 196, 1) 100%
+  );
+  color: #fff;
+  padding: 50px 0;
+}
+.con_popular .row {
+  display: flex;
+  align-items: stretch;
+  margin: 0;
+}
+.con_popular .row .con_popular_item_wrapper {
+  width: 32%;
+}
+.con_popular .row .con_popular_item_wrapper:nth-child(2) {
+  width: 32%;
+  margin: 0 30px;
+}
+.con_popular h2 {
+  margin: 0;
+  color: #fff;
+  font-size: 34px;
+  padding-bottom: 35px;
+  font-family: "Proxima Nova", sans-serif;
+  font-weight: 400;
+}
+.con_popular a.con_popular_item {
+  display: block;
+  padding: 28px 31px;
+  background-color: #448fe3;
+  color: #fff;
+  border-radius: 10px;
+  height: 100%;
+  transition: all 0.3s ease-in-out;
+}
+.con_popular a.con_popular_item:hover,
+.con_popular a.con_popular_item:focus {
+  background: linear-gradient(126.61deg, #65acfc 0%, #2d83e3 100%);
+  box-shadow: 0px 4px 24px rgba(0, 0, 0, 0.15);
+  transform: scale(1.02, 1.02);
+}
+.con_popular a.con_popular_item > img {
+  margin-bottom: 20px;
+}
+.con_popular a.con_popular_item p {
+  margin: 0;
+  font-size: 20px;
+  font-family: "Proxima Nova", sans-serif;
+  font-weight: 400;
+  max-width: 260px;
+}
+@media (max-width: 767px) {
+  .con_popular h2 {
+    font-size: 28px;
+    padding-bottom: 26px;
+  }
+  .con_popular .row {
+    flex-direction: column;
+  }
+  .con_popular .row .con_popular_item_wrapper:nth-child(2) {
+    width: 100%;
+    margin: 30px 0px;
+  }
+  .con_popular .row .con_popular_item_wrapper {
+    width: 100%;
+  }
+  .con_popular a.con_popular_item p {
+    font-size: 18px;
+  }
+  .con_popular a.con_popular_item {
+    padding: 18px 25px;
+  }
+}
+
+.con_about {
+  padding: 120px 0 90px 0;
+}
+.con_about .row {
+  display: flex;
+  align-items: stretch;
+}
+.con_about .con_container > .row:first-of-type {
+  margin-bottom: 20px;
+}
+.con_about_start {
+  padding: 30px 20px;
+  background: #eaf5fe;
+  border-radius: 10px;
+  box-shadow: 0px 0px 7px rgb(0 0 0 / 20%);
+}
+.con_about_start h2 {
+  margin: 0;
+  padding-bottom: 10px;
+  font-weight: 400;
+  font-size: 30px;
+  font-family: "Proxima Nova", sans-serif;
+}
+.con_about_start p {
+  font-family: "Proxima Nova", sans-serif;
+  font-weight: 400;
+  font-size: 16px;
+}
+.con_about_learn {
+  box-shadow: 0px 0px 7px rgb(0 0 0 / 15%);
+  border-radius: 10px;
+  padding: 30px;
+}
+.con_about_start,
+.con_about_learn {
+  height: 100%;
+}
+.con_learn_img {
+  border-right: 1px solid #e7e7e7;
+}
+.con_learn_img h3 {
+  margin: 0;
+  padding-bottom: 20px;
+  font-size: 20px;
+  font-family: "Proxima Nova", sans-serif;
+}
+
+.con_learn_info a {
+  color: #000;
+  font-weight: 400;
+  font-family: "Proxima Nova", sans-serif;
+  display: block;
+  margin-bottom: 18px;
+  transition: all 0.3s ease-in-out;
+}
+.con_learn_info a:hover {
+  color: #1467c5;
+}
+.con_learn_info a:focus {
+  font-weight: 600;
+}
+.con_learn_info a:last-of-type {
+  margin-bottom: 0;
+}
+.con_about_start_head {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #e7e7e7;
+  padding-bottom: 15px;
+  margin-bottom: 20px;
+}
+.con_about_start_head h2 {
+  padding-left: 15px;
+  font-size: 20px;
+  font-family: "Proxima Nova", sans-serif;
+  font-weight: 700;
+  margin: 0;
+}
+
+@media (max-width: 767px) {
+  .con_about .row {
+    flex-direction: column;
+  }
+  .con_learn_img {
+    display: flex;
+    align-items: center;
+    border-right: none;
+    border-bottom: 1px solid #e7e7e7;
+    margin-bottom: 20px;
+    padding-bottom: 15px;
+  }
+  .con_learn_img img {
+    order: 1;
+    max-width: 70px;
+  }
+  .con_about_start_head im {
+    max-width: 70px;
+  }
+  .con_learn_img h3 {
+    order: 2;
+  }
+
+  .con_about {
+    padding: 50px 10px 20px;
+  }
+  .con_about .con_container > .row:first-of-type {
+    margin-bottom: 0px;
+  }
+  .con_about div.col-sm-12 {
+    margin-bottom: 20px;
+  }
+}
+
+
+</style>
+`)
