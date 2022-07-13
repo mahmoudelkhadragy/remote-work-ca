b8d06e18792d437c75ebeee519504e0aa5d2a6e3
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2022-07-13 23:32:16 +0200

    add scroll to and click functionality

--- BWW/BWW-009/index.js
+++ BWW/BWW-009/index.js
@@ -4,6 +4,9 @@ convert.$('head').append(`
 body{
   background: #E5E5E5 !important;
 }
+.search_box{
+  z-index: 999;
+}
 .con_sidebar{
   width: calc((calc(100% - 980px))/2);
   position: fixed;
@@ -12,8 +15,9 @@ body{
   background-color: #fff;
   font-family: "Proxima Nova", sans-serif;
   border-radius: 6px;
+  transition: all .2s ease-in-out;
 }
-.con_sidebar p{
+.con_sidebar > p{
   margin: 0;
   background-color: #2579C6;
   color: #fff;
@@ -23,35 +27,57 @@ body{
   font-size: 16px;
   font-family: "Proxima Nova", sans-serif;
 }
-.con_sidebar ul{
-  list-style: none;
+#con_wrapper{
+  max-height: 60vh;
+  overflow: auto;
+}
+#con_wrapper p{
+  font-size: 14px;
+  padding: 9px 10px 7px 15px;
+  cursor: pointer;
+  color: #000;
+  font-family: "Proxima Nova", sans-serif;
+  font-weight: 400;
+  transition: all .2s ease-in-out;
   margin: 0;
-  padding: 0;
-}
-.con_sidebar ul li{
   border-bottom: 1px solid #E9E9E9;
-  line-height: 22px !important;
 }
-.con_sidebar ul li:last-child{
+#con_wrapper p:last-child{
   border-bottom: none;
 }
 
-.con_sidebar ul li a{
-  font-size: 14px;
-  padding: 9px 10px 7px 15px;
-  display: block;
-  cursor: pointer;
-  color: #000;
-  font-weight: 400;
-  font-family: "Proxima Nova", sans-serif;
-  transition: all .2s ease-in-out;
+#con_wrapper p:hover,
+#con_wrapper p:focus{
+  padding: 9px 10px 7px 20px;
+  background-color: #DDE5EC;
 }
 
-.con_sidebar ul li a:hover,
-.con_sidebar ul li a:focus{
-  padding: 9px 10px 7px 21px;
+#con_wrapper::-webkit-scrollbar {
+  width: 7px;
+}
+
+/* Track */
+#con_wrapper::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 5px #ccc; 
+  border-radius: 10px;
+}
+ 
+/* Handle */
+#con_wrapper::-webkit-scrollbar-thumb {
+  background: #777; 
+  border-radius: 10px;
+}
+
+/* Handle on hover */
+#con_wrapper::-webkit-scrollbar-thumb:hover {
+  background: #666; 
+}
+
+.top_stick{
+  top: 20px;
+}
+.con_active{
   background-color: #DDE5EC;
-  text-decoration: none;
 }
 </style>
 `);
@@ -60,24 +86,48 @@ body{
 const allChapterLinks = convert.$('.country-chapter .chapter-link');
 let allLi = ``
 
-allChapterLinks.each(function(i){
-    let linkTitle = $(this).text();
-    $(this).attr('data-link', `link_${i}`);
-    let list = `
-    <li>
-      <a data-link="link_${i}">${linkTitle}</a>
-    </li>
-    `
-    allLi += list;
+allChapterLinks.each(function (i) {
+  let linkTitle = $(this).text();
+  let list = '';
+  $(this).attr('data-link', `link_${i}`);
+
+  list = `<p data-link="link_${i}">${linkTitle}</p>`;
+
+  allLi += list;
 });
 
-convert.$('.container').append(`
+
+
+convert.$('body').append(`
 <div class="con_sidebar">
   <p>Table of Contents</p>
-  <ul id="con_ul">
+  <div id="con_wrapper">
     ${allLi}
-  </ul>
+  </div>
 </div>
 `);
 
 
+convert.$('#con_wrapper p').on('click', function () {
+  let dataLink = convert.$(this).data('link');
+  $(this).addClass('con_active');
+  $(this).siblings().removeClass('con_active');
+  $([document.documentElement, document.body]).animate({
+    scrollTop: $(`.country-chapter .chapter-link[data-link="${dataLink}"]`).offset().top - 48
+  }, 1000);
+});
+
+
+$(window).scroll(function () {
+  let height = convert.$(window).scrollTop();
+  let target = convert.$('.con_sidebar');
+  if (height > '180') {
+    if (!target.hasClass('top_stick')) {
+      target.addClass('top_stick');
+    }
+  } else {
+    if (target.hasClass('top_stick')) {
+      target.removeClass('top_stick');
+    }
+  }
+});
